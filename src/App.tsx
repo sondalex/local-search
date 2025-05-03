bc56fc42ae8ed7b220ee6f71a3d977fd2075aba9
sondalex <61547150+sondalex@users.noreply.github.com>
2025-05-03 09:49:34 +0200

    Add system theme support

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ModelSelector from '@/components/modelSelector'
 import { useState } from 'react'
 import { Model } from '@/lib/types'
 import Footer from './components/footer'
+import { ThemeProvider } from './context/Theme'
 
 export default function App() {
     const [model, setModel] = useState<Model | null>(null)
@@ -13,21 +14,23 @@ export default function App() {
     }
     return (
         <div className="min-h-screen flex flex-col">
-            <main className="container mx-auto px-4 py-8 flex-grow">
-                <h1 className="text-3xl font-bold mb-8 text-center">
-                    Local Search
-                </h1>
-                <DuckDBProvider>
-                    {model ? (
-                        <SearchInterface model={model} />
-                    ) : (
-                        <ModelSelector
-                            onModelSelect={handleOnModelSelect}
-                        ></ModelSelector>
-                    )}
-                </DuckDBProvider>
-            </main>
-            <Footer githubURL="https://github.com/sondalex/local-search"></Footer>
+            <ThemeProvider>
+                <main className="container mx-auto px-4 py-8 flex-grow">
+                    <h1 className="text-3xl font-bold mb-8 text-center">
+                        Local Search
+                    </h1>
+                    <DuckDBProvider>
+                        {model ? (
+                            <SearchInterface model={model} />
+                        ) : (
+                            <ModelSelector
+                                onModelSelect={handleOnModelSelect}
+                            ></ModelSelector>
+                        )}
+                    </DuckDBProvider>
+                </main>
+                <Footer githubURL="https://github.com/sondalex/local-search"></Footer>
+            </ThemeProvider>
         </div>
     )
 }
--- src/context/context.ts
+++ src/context/context.ts
@@ -1,10 +1,20 @@
 import { useContext } from 'react'
 import { DuckDBContext, DuckDBContextType } from './DuckDB'
 import { ExpectedSchemaContext, ExpectedSchemaType } from './ExpectedSchema'
+import { ThemeProviderContext } from './Theme'
 
 const useDuckDB = (): DuckDBContextType => useContext(DuckDBContext)
 
 const useExpectedSchema = (): ExpectedSchemaType =>
     useContext(ExpectedSchemaContext)
 
-export { useDuckDB, useExpectedSchema }
+const useTheme = () => {
+    const context = useContext(ThemeProviderContext)
+
+    if (context === undefined)
+        throw new Error('useTheme must be used within a ThemeProvider')
+
+    return context
+}
+
+export { useDuckDB, useExpectedSchema, useTheme }
